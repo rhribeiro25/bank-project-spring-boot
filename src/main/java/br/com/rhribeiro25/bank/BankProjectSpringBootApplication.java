194a16c06d6ad9c2257eb910866c17f744f9fbbc
Renan Ribeiro <renan.ribeiro@dasa.com.br>
2020-11-26 19:49:21 -0300

    removing logging configurations XML and adding log4j2 configurations properties

--- src/main/java/br/com/rhribeiro25/bank/BankProjectSpringBootApplication.java
+++ src/main/java/br/com/rhribeiro25/bank/BankProjectSpringBootApplication.java
@@ -9,10 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
 @EntityScan(basePackages = { "br.com.rhribeiro25.bank.model.entity" })
@@ -23,10 +20,6 @@ public class BankProjectSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankProjectSpringBootApplication.class, args);
 		log.info("Starting Bank!");
-		log.error("Starting Bank!");
-		log.debug("Starting Bank!");
-		log.warn("Starting Bank!");
-		log.info("Simple log statement with inputs {}, {} and {}", 1, 2, 3);
 	}
 
 	@Bean
